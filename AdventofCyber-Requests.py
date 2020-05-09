7ee676c239cd72288e1eb03f64dce64678a1e0f6
Regaip KURT <regkrt@gmail.com>
2020-05-09 03:07:19 +0300

    TryHackMe-Task14

--- AdventofCyber-Requests.py
+++ AdventofCyber-Requests.py
@@ -2,14 +2,33 @@ import requests
 import sys
 url = "http://10.10.169.100:3000/"
 
+#sonucu daha sonra strşnge çevirmnek yerine direk string tanımlayıp atama yapılırsa daha kısa olur.
+liste = [] 
+
 try:
     while True:
+
         sonuc = requests.get(url)
         deger = eval(sonuc.content)
+        
+        if deger["value"]=="end":
+        	print("Y")
+        	liste.append("Y")
+        	break
         print(str(deger["value"]))
+        liste.append(deger["value"])
+
         if url == "http://10.10.169.100:3000/":
-            url = url + deger["next"]
+            url = url + deger["next"]        	
         else:
             url = url[:-1] + deger["next"]
 except:
-    sys.exit()
+	print("Bir hata oluştu!")
+
+enson = ""
+
+for i in liste:
+	enson = enson + i
+
+print("\n\n Sonuc: ", enson)
+
