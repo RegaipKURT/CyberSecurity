f14661da2466c667991be2d7097434442c30359c
Regaip KURT <regkrt@gmail.com>
2020-05-13 19:29:58 +0300

    Update Request_Get_isteği.py

--- Request_Get_isteği.py
+++ Request_Get_isteği.py
@@ -1,6 +1,5 @@
 """
 Advent of Scyber isimli tryhackme odasında bir challenge için yazılan bu kod ile
-
 bir site içinden gelen değerler ile başka bir sayfaya gidilip o sayfanın içeriği alınıyor.
 get isteği yapıp sayfadaki içeriğe göre yeni url'ye get isteği atan çok basit bir kod örneği.
 """
@@ -10,14 +9,15 @@ import sys
 url = "http://10.10.169.100:3000/"
 
 #sonucu daha sonra stringe çevirmnek yerine direk string tanımlayıp atama yapılırsa daha kısa olur.
-liste = [] 
+liste = []
 
 try:
     while True:
 
         sonuc = requests.get(url)
-        deger = eval(sonuc.content)
-        
+        deger = eval(sonuc.content) # eval(), string içeriği sözlük tipine çevirir
+        #sayfadaki içerik sözlüğe çevrilebilecek bir string veya json objesi diyebiliriz.
+	
         if deger["value"]=="end":
         	print("Y")
         	liste.append("Y")
