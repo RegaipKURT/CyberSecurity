40e9082e6a3932eec7c79a536ad3474317d1b3c6
Regaip KURT <regkrt@gmail.com>
2020-05-27 15:59:24 +0300

    Wifi Şifresi Öğrenme - Linux

--- wifi_sifre_alma.py
+++ wifi_sifre_alma.py
@@ -3,12 +3,34 @@
 Bu program sadece linux icin yazılmıştır.
 Çalışmak için yönetici hakları ister.
 """
+# ÜZERİNDE ÇALIŞACAĞIMIZ LİNUX KONFİGRASYON DOSYASI AŞAĞIDAKİ GİBİ GÖRÜNÜYOR.
+# ŞU KOMUTLA ULAŞILABİLİR: sudo cat /etc/NetworkManager/system-connections/*
+"""
+[wifi]
+mac-address-blacklist=
+mode=infrastructure
+ssid=BAĞLANTININ ADI
+[wifi-security]
+auth-alg=open
+key-mgmt=wpa-psk
+psk=PAROLA
+
+[wifi]
+mac-address-blacklist=
+mode=infrastructure
+ssid=BAĞLANTININ ADI
+[wifi-security]
+key-mgmt=wpa-psk
+psk=PAROLA
+"""
 
 import os
 import sys
 
 #gerekli komutu çalıştırıp tmp içinde sonuc.txt'ye kaydediyoruz.
+#tmp dizini geçici dosyaları barındırır. Bilgisayar kapandığında bu dosyalar otomatik silinir.
 if str(os.name).startswith("pos"):
+	#yaptığımız bütün bağlantıları bir sonuç dosyasına yazdıralım
 	str(os.system("sudo cat /etc/NetworkManager/system-connections/* > /tmp/sonuc.txt"))
 else:
 	print("Bu program sadece linux işletim sistemi üzerinde çalışmaktadır.")
@@ -17,7 +39,10 @@ else:
 #sonuc.txt içinden okuyarak aradığımız değerleri bulup yazdıralım.
 with open("/tmp/sonuc.txt", "r") as sonuc_dosyası:
 	degerler = sonuc_dosyası.readlines()
+	#bağlantılar içindeki konfigrasyon ayarlarından şifrelere ve bağlantının adına ait olanları bulalım
+	#sonuç dosyasının içindeki satırlarda teker teker ilerleyelim.
 	for deger in degerler:
+		# "=" işaretine göre ayırıp id ve psk'ya karşılık gelen değerleri bulacağız. 
 		sonuc = str(deger).strip("=")
 		if sonuc[:2] =="id":
 			sonucum = sonuc[3:-1] + ": "
@@ -26,4 +51,5 @@ with open("/tmp/sonuc.txt", "r") as sonuc_dosyası:
 			print(sonuc[4:-1]) 
 
 #en son kullandığımız geçici dosyayı siliyoruz.
+#silmesek bile bilgisayar yueniden başladığında kendiliğinden silinecek(tmp içinde olduğu için)
 os.system("rm /tmp/sonuc.txt")
